4b95f33e6d1a866e8b804ea9463e4f1ba35fd59e
thecraftman <temitopetola@gmail.com>
2022-07-18 00:14:40 +0100

    update opsyml

--- index.js
+++ index.js
@@ -28,44 +28,7 @@ connection.connect(function(err) {
 */
 const api = express()
 api.get('/', (req, res) => {
-  res.send('Workflow mining in progress
-  {
-      "blockchain": [
-          {
-              "index": 0,
-              "timestamp": "01/01/2020",
-              "data": "Initial Block in the Chain",
-              "precedingHash": "0",
-              "hash": "f556af85a25f2883d26b011d041ab717f3a82f6c9e2d8bd897510c13ec14e61f",
-              "nonce": 0
-          },
-          {
-              "index": 1,
-              "timestamp": "13/07/2022",
-              "data": {
-                  "sender": "CTO.ai",
-                  "recipient": "Workflow",
-                  "quantity": 100
-              },
-              "precedingHash": "f556af85a25f2883d26b011d041ab717f3a82f6c9e2d8bd897510c13ec14e61f",
-              "hash": "0000ffaf7b9ae573a9cb3ba606b78dc45a8944ce6ffb0fcbee505b942d8b3e05",
-              "nonce": 65066
-          },
-          {
-              "index": 2,
-              "timestamp": "03/06/2021",
-              "data": {
-                  "sender": "Salman Jonah",
-                  "recipient": "pol king",
-                  "quantity": 100
-              },
-              "precedingHash": "0000ffaf7b9ae573a9cb3ba606b78dc45a8944ce6ffb0fcbee505b942d8b3e05",
-              "hash": "0000e135682138fe8a248e69863b80dcfce1dbc29957a02b861ea1609b4b60d6",
-              "nonce": 15555
-          }
-      ],
-      "difficulty": 4
-  }\n')
+  res.send('"precedingHash": "f556af85a25f2883d26b011d041ab717f3a82f6c9e2d8bd897510c13ec14e61f","hash": "0000ffaf7b9ae573a9cb3ba606b78dc45a8944ce6ffb0fcbee505b942d8b3e05","nonce": 65066\n')
 })
 
 api.listen(PORT, HOST)
